ee8d228a1cfc15d9977186362d4932fdfb6ca588
armando <abpprkonsalting@gmail.com>
2022-09-01 13:46:45 -0400

    fix in migration

--- migrations/Version20220901162032.php
+++ migrations/Version20220901162032.php
@@ -20,15 +20,15 @@ final class Version20220901162032 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, productType_id INT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_D34A04AD5E032AB4 (productType_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, product_type_id INT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_D34A04AD14959723 (product_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE product_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD5E032AB4 FOREIGN KEY (productType_id) REFERENCES product_type (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD14959723 FOREIGN KEY (product_type_id) REFERENCES product_type (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD5E032AB4');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD14959723');
         $this->addSql('DROP TABLE product');
         $this->addSql('DROP TABLE product_type');
     }
